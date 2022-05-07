85315906701d63b829907dba30d277f959269a14
Szymon Śliwa <szymon242820@gmail.com>
2022-05-07 02:28:09 +0200

    express server

--- auth/src/Components/Login/Login.js
+++ auth/src/Components/Login/Login.js
@@ -1,16 +1,40 @@
+import React from 'react';
 import { Form, Col, Button } from 'react-bootstrap';
+import PropTypes from 'prop-types';
 
-function Login(){
+async function loginUser(credentials){
+    return fetch('http://localhost:8080/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(credentials)
+    }).then(data => data.json())
+}
+
+function Login({setToken}){
+    const [username, setUsername] = React.useState();
+    const [password, setPassword] = React.useState();
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        const token = await loginUser({
+            username,
+            password
+        });
+        setToken(token);
+    }
+
     return (
         <div className="Login">
             <h1>Please Log In</h1>
-            <Form>
+            <Form onSubmit={ handleSubmit }>
                 <Form.Group controlId="username">
                     <Form.Label column>
                         Username
                     </Form.Label>
                     <Col>
-                        <Form.Control type="text" placeholder="Username" />
+                        <Form.Control type="text" placeholder="Username" onChange={e=>setUsername(e.target.value)} />
                     </Col>
                 </Form.Group>
                 <Form.Group controlId="password">
@@ -18,7 +42,7 @@ function Login(){
                         Password
                     </Form.Label>
                     <Col>
-                        <Form.Control type="password" placeholder="Password" />
+                        <Form.Control type="password" placeholder="Password" onChange={e=>setPassword(e.target.value)} />
                     </Col>
                 </Form.Group>
                 <Form.Group>
@@ -29,6 +53,10 @@ function Login(){
             </Form>
         </div>
     );
+}
+
+Login.propTypes = {
+    setToken: PropTypes.func.isRequired
 }
 
 export default Login;
